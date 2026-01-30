df56792bf6e1236ab3e9c12bc69de42341a64eda
coding1100 <coding1343453@gmail.com>
2026-01-30 18:26:05 +0500

    desktop & mobile speed 00

--- app/components/OtherLandingPages/UsExpert/ExpertSection.tsx
+++ app/components/OtherLandingPages/UsExpert/ExpertSection.tsx
@@ -3,9 +3,9 @@
 import Image, { StaticImageData } from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-// slick-theme.css removed - loads heavy font file, styles in globals.css
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useRef, FC } from "react";
+import styles from "./ExpertSection.module.css";
 
 interface ExpertSectionProps {
   content: {
@@ -72,8 +72,8 @@ const ExpertSection: FC<ExpertSectionProps> = ({ content }) => {
           </p>
         </div>
 
-        {/* Expert Slider */}
-        <div className="relative">
+        {/* Expert Slider – dots styled in ExpertSection.module.css */}
+        <div className={`relative ${styles.expertSliderRoot}`}>
           <Slider ref={sliderRef} {...settings}>
             {content.slider.map((expert, index) => (
               <div key={index} className="px-5 h-full">
@@ -99,7 +99,7 @@ const ExpertSection: FC<ExpertSectionProps> = ({ content }) => {
           </Slider>
 
           {/* Navigation Arrows */}
-          <div className="w-[225px] mx-auto flex justify-around mt-[5px] relative z-[9]">
+          <div className="w-[295px] mx-auto flex justify-around -mt-[31px] relative z-[9]">
             <ChevronLeft
               size={20}
               className="cursor-pointer"
