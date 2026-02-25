28569e3253c2f7ef1fb68f78cad0776b39a5f567
MTouheedJ <touheedmirza096@gmail.com>
2026-02-25 13:13:14 +0500

    new changings

--- app/components/ClientScripts.tsx
+++ app/components/ClientScripts.tsx
@@ -60,43 +60,18 @@ export default function ClientScripts() {
     });
   }, []);
 
-  const showLiveChatWidget = useCallback(() => {
-    if (typeof window === "undefined") return;
-
-    const selectors = [
-      '#livechat-container',
-      '[id*="livechat"]',
-      '[class*="livechat"]',
-      '[id*="LiveChat"]',
-      '[class*="LiveChat"]'
-    ];
-
-    selectors.forEach(selector => {
-      try {
-        const elements = document.querySelectorAll(selector);
-        elements.forEach((el) => {
-          if (el instanceof HTMLElement) {
-            el.style.display = "";
-            el.style.visibility = "";
-          }
-        });
-      } catch (e) {
-        // Ignore selector errors
-      }
-    });
-  }, []);
-
-  // Show LiveChat widget on all pages
+  // Hide LiveChat when not on thank-you (e.g. after navigating away from thank-you)
   useEffect(() => {
     if (typeof window === "undefined") return;
-
-    // Show LiveChat widget on all pages
-    showLiveChatWidget();
-  }, [currentPage, showLiveChatWidget]);
+    if (!isThankYouPage) {
+      hideLiveChatWidget();
+    }
+  }, [currentPage, isThankYouPage, hideLiveChatWidget]);
 
   return (
     <>
-      {/* LiveChat - visible on all pages, loaded lazily */}
+      {/* LiveChat - load script only on thank-you page */}
+      {isThankYouPage && (
       <Script
         id="livechat-script"
         strategy="lazyOnload"
@@ -109,6 +84,7 @@ export default function ClientScripts() {
           ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
         `}
       </Script>
+      )}
 
       {/* HelpCrunch - only on /about-us/ page, loaded lazily */}
       {isAboutPage && (
