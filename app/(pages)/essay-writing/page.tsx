6df1f303ef5e9d94d8552c0fb821390eb6ae6df4
Mohsin Riaz <coding1343453@gmail.com>
2026-05-05 12:07:30 +0500

    Revert "schema added"

--- app/(pages)/essay-writing/page.tsx
+++ app/(pages)/essay-writing/page.tsx
@@ -6,8 +6,6 @@ import Subjects from "@/app/components/LandingPage/Subjects";
 import { EssayWritingDataProvider } from "./EssayWritingDataProvider";
 import { essayWritingSubjects } from "./content";
 import { getPageData } from "@/app/lib/mongodb";
-import JsonLdHead from "@/app/components/Seo/JsonLdHead";
-import { buildServiceJsonLd, normalizeSiteUrl } from "@/app/lib/serviceJsonLd";
 
 // Force dynamic rendering to prevent caching
 export const dynamic = "force-dynamic";
@@ -36,19 +34,9 @@ async function fetchEssayWritingData() {
 
 const Page = async () => {
   const pageData = await fetchEssayWritingData();
-  const baseUrl = normalizeSiteUrl(
-    process.env.NEXT_PUBLIC_SITE_URL || "https://scholarlyhelp.com",
-  );
-  const schemaJson = buildServiceJsonLd({
-    pageData,
-    canonicalUrl: `${baseUrl}/essay-writing/`,
-    title: MetaData.essayWriting.title,
-    description: MetaData.essayWriting.description,
-  });
 
   return (
     <EssayWritingDataProvider data={pageData}>
-      <JsonLdHead id="essay-writing-service-json-ld" json={schemaJson} />
       <MainLayout>
         <HeroSection />
         <BelowFoldLanding>
